c0da4142cfd1b81530119c43a27300ed73599ed9
Anton-Malyugin <a.malyugin@mute-lab.com>
2018-06-17 11:38:29 +0400

    edit popup for issued meal

--- src/components/popup-meal-rossiya/popup-meal-rossiya.js
+++ src/components/popup-meal-rossiya/popup-meal-rossiya.js
@@ -18,7 +18,8 @@ angular.module('app').component('popupMealRossiya', {
 		selectFlightPassenger: '=select',
 		getLimitStatus: '=limit',
 		mealSelect: '=',
-		getMealInfo: '='
+		getMealInfo: '=',
+		issuedMeal: '='
 	}
 });
 
@@ -98,24 +99,18 @@ function PopupMealRossiyaController(utils, $scope) {
 	function switchNext() {
 		var successSwitch = false;
 		for (var i = vm.currentPassengerIndex + 1; i < vm.passengers.length; i++) {
-			if (
-				vm.service.availableByPassengerSegments[i] &&
-				vm.service.availableByPassengerSegments[i][vm.currentFlightIndex]
-			) {
+
 				vm.selectFlightPassenger(vm.currentFlightIndex, i);
 				successSwitch = true;
 				break;
-			}
+
 		}
 		if (!successSwitch) {
 			for (var j = 0; j < vm.passengers.length; j++) {
-				if (
-					vm.service.availableByPassengerSegments[j] &&
-					vm.service.availableByPassengerSegments[j][vm.currentFlightIndex]
-				) {
+
 					vm.selectFlightPassenger(vm.currentFlightIndex, j);
 					break;
-				}
+
 			}
 		}
 	}
@@ -123,24 +118,18 @@ function PopupMealRossiyaController(utils, $scope) {
 	function switchPrev() {
 		var successSwitch = false;
 		for (var i = vm.currentPassengerIndex - 1; i > -1; i--) {
-			if (
-				vm.service.availableByPassengerSegments[i] &&
-				vm.service.availableByPassengerSegments[i][vm.currentFlightIndex]
-			) {
+
 				vm.selectFlightPassenger(vm.currentFlightIndex, i);
 				successSwitch = true;
 				break;
-			}
+
 		}
 		if (!successSwitch) {
 			for (var j = vm.passengers.length - 1; j > -1; j--) {
-				if (
-					vm.service.availableByPassengerSegments[j] &&
-					vm.service.availableByPassengerSegments[j][vm.currentFlightIndex]
-				) {
+
 					vm.selectFlightPassenger(vm.currentFlightIndex, j);
 					break;
-				}
+
 			}
 		}
 	}
